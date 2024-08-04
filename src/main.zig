2d385c2745288f40a00f90e4090535243d2acfb7
Adil <adilahmeddev@gmail.com>
2024-08-04 11:53:59 +0100

    reading request bodies

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,7 @@ pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("Server starting...\n", .{});
 
-    const serv_addr = try std.net.Address.resolveIp("0.0.0.0", 6767);
+    const serv_addr = try std.net.Address.parseIp4("127.0.0.1", 6767);
     var listener = try serv_addr.listen(.{ .reuse_address = true });
 
     std.debug.print("Server started listening on {}\n", .{serv_addr});
@@ -17,6 +17,7 @@ pub fn main() !void {
         while (conn.stream.read(req_body[req_body_total..])) |reqBodyLength| {
             if (reqBodyLength == 0) break;
             req_body_total += reqBodyLength;
+
             if (std.mem.containsAtLeast(u8, req_body[0..req_body_total], 1, "\r\n\r\n")) {
                 break;
             }
@@ -29,12 +30,19 @@ pub fn main() !void {
             continue;
         }
 
-        var headerIterator = std.mem.tokenizeSequence(u8, recv_data, "\r\n");
-        while (headerIterator.next()) |line| {
-            var headerLine = std.mem.split(u8, line, ":");
-            std.debug.print("{s}\n", .{headerLine.first()});
-            std.debug.print("{s}\n", .{headerLine.rest()});
-            std.debug.print("------------\n", .{});
+        var split_data = std.mem.splitSequence(u8, recv_data, "\r\n\r\n");
+        if (split_data.next()) |headers| {
+            var headerIterator = std.mem.tokenizeSequence(u8, headers, "\r\n");
+
+            while (headerIterator.next()) |line| {
+                var headerLine = std.mem.splitAny(u8, line, ":");
+                std.debug.print("{s}\n", .{headerLine.first()});
+                std.debug.print("{s}\n", .{headerLine.rest()});
+                std.debug.print("------------\n", .{});
+            }
+        }
+        if (split_data.next()) |body| {
+            std.debug.print("|{s}|\n", .{body});
         }
     } else |err| {
         std.debug.print("error while accepting connection {}\n", .{err});
